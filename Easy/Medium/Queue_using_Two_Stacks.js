0b9080f3c6885b225a8c3c3f3b9f54945631c480
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-10-12 16:33:09 -0400

    Queues

---
+++ Easy/Medium/Queue_using_Two_Stacks.js
@@ -0,0 +1,29 @@
+
+class QueueUsingTwoStacks {
+  constructor() {
+    this.input = [];
+    this.output = [];
+  }
+
+  enqueue(x) {
+    this.input.push(x);
+  }
+
+  dequeue() {
+    this.peek();
+    return this.output.pop();
+  }
+
+  peek() {
+    if (this.output.length === 0) {
+      while (this.input.length > 0) {
+        this.output.push(this.input.pop());
+      }
+    }
+    return this.output[this.output.length - 1];
+  }
+
+  isEmpty() {
+    return this.input.length === 0 && this.output.length === 0;
+  }
+}
